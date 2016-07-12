a9a0bacec2f0da32632c4cb49c5e2ee9e8a870ed
saeideh-sh <saeede.shahrokh@gmail.com>
2016-07-11 21:11:24 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-# this was created by Saeideh Shahrokh Esfahani
+# this was created by Saeideh Shahrokh Esfahani (July 11- 2016)
 python ./src/insight_challenge_time.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
